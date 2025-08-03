c30f8215590de81a66f0be7911f28ac45657fb46
gormar108 <gormar108@gmail.com>
2025-08-03 15:51:09 +0200

    Made a pick a shape scene

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
     public void Play()
     {
-
+        SceneManager.LoadScene(1);
     }
 
     public void Quit()
